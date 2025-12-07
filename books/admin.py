963edf0042a91c9bf1e22e97ea4d0ec1403a77b0
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-07 18:51:16 +0100

    update books/admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'isbn', 'available_copies', 'total_copies', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'author', 'isbn')
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        ('Book Details', {
+            'fields': ('title', 'author', 'isbn', 'published_date')
+        }),
+        ('Inventory', {
+            'fields': ('total_copies', 'available_copies')
+        }),
+        ('Metadata', {
+            'fields': ('created_at',),
+            'classes': ('collapse',)
+        }),
+    )
