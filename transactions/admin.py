5341c10f3fdf02b780321fdd222c0a2b66b0118b
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-07 18:57:08 +0100

    update transactions/admin.py

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'transaction_type', 'transaction_date', 'due_date', 'returned_date')
+    list_filter = ('transaction_type', 'transaction_date')
+    search_fields = ('user__email', 'book__title')
+    readonly_fields = ('transaction_date',)
+    date_hierarchy = 'transaction_date'
