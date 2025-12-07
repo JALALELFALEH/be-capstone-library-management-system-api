0e098cf2a7063bcf0d53cadf0adb48d79eae4471
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-07 18:53:35 +0100

    update users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import LibraryUser
 
-# Register your models here.
+@admin.register(LibraryUser)
+class LibraryUserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'date_of_membership', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'date_of_membership')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined', 'date_of_membership')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
